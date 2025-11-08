3fd34673e1fd4119c9dec25f0ae1bfc46d62d59f
hyoga-dev <hyogadecaprio4@gmail.com>
2025-11-08 19:48:04 +0700

    setup

---
+++ src/hello.js
@@ -0,0 +1,14 @@
+function Hello () {
+  return (
+    <><p>my name is udin</p></>
+  )
+}
+
+function Hillo () {
+  return (
+    <><p>my name is idin</p></>
+  )
+}
+
+export default Hello;
+export {Hillo};
